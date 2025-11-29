8481cf89ef16c241e3d21ba3c01d23436cbc8ca7
lgreer929 <lisa.greer@comcast.net>
2025-11-28 17:46:46 -0700

    Fixing Charts Again

--- hr_dashboard.py
+++ hr_dashboard.py
@@ -114,7 +114,7 @@ with colA:
         filtered,
         names = "Race/Descent",
         title="Race Distribution",
-        color_discrete_sequence=px.colors.qualitative.Bold
+        color_discrete_sequence=px.colors.qualitative.Set3
     )
     st.plotly_chart(fig_race, use_container_width=True)
 
